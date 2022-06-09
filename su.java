7e73f8086ab26c0a6497f4892c0e356c8d759ecb
Pranay Goyal <pranaygo1234@gmail.com>
2022-06-09 21:58:35 +0530

    Java programs added

---
+++ su.java
@@ -0,0 +1,9 @@
+public class su
+{
+    public static void main(String [] args) {
+        int i = 10;
+        int a=20;
+        System.out.print("multiple");
+        System.out.println(i*a);
+    }
+}
--- sum.cpp
+++ sum.cpp
@@ -3,5 +3,5 @@ using namespace std;
 int main()
 {
     int a=10,b=10;
-    cout<<a+b;
+    cout<<"SUM:: "<<a+b;
 }
